146d5a64dc95a9af949b3d957591babfd449ea7f
haruka.kawabe <hrk.kwb12185738@gmail.com>
2020-11-27 17:02:52 +0900

    Add User class

---
+++ src/models/User.java
@@ -0,0 +1,81 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "users")
+@NamedQueries({
+        @NamedQuery(name = "checkRegisteredMailAddress", query = "SELECT COUNT(u) FROM User AS u WHERE u.mail_address= :mail_address"),
+
+        @NamedQuery(name = "checkLoginMailAddressAndPassword", query = "SELECT u FROM User AS u WHERE u.mail_address= :mail_address AND u.password= :password"
+
+        )
+
+})
+
+@Entity
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "mail_address", nullable = false, unique = true)
+    private String mail_address;
+
+    @Column(name = "password", length = 64, nullable = false)
+    private String password;
+
+    @Column(name = "image", length = 255, nullable = false)
+    private String image;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail_address() {
+        return mail_address;
+    }
+
+    public void setMail_address(String mail_address) {
+        this.mail_address = mail_address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
---
+++ src/models/validators/UserValidator.java
@@ -0,0 +1,71 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import models.User;
+import utils.DBUtil;
+
+public class UserValidator {
+    public static List<String> validate(User u,Boolean mail_address_duplicate_check_flag,Boolean password_check_flag){
+        List<String> errors=new ArrayList<String>();
+
+        String mail_address_error= _validateMail_address(u.getMail_address(),mail_address_duplicate_check_flag);
+        if(!mail_address_error.equals("")){
+            errors.add(mail_address_error);
+        }
+
+        String name_error = _validateName(u.getName());
+        if(!name_error.equals("")){
+            errors.add(name_error);
+        }
+
+        String password_error= _validatePassword(u.getPassword(),password_check_flag);
+        if(!password_error.equals("")){
+            errors.add(password_error);
+        }
+        return errors;
+
+
+    }
+
+    //メールアドレス
+    private static String _validateMail_address(String mail_address,Boolean mail_address_duplicate_check_flag){
+        //必須入力チェック
+        if(mail_address==null||mail_address.equals("")){
+            return "メールアドレスを入力してください。";
+        }
+        //すでに登録されているメールアドレスとの重複チェック
+        if(mail_address_duplicate_check_flag){
+            EntityManager em =DBUtil.createEntityManager();
+            long user_count = (long)em.createNamedQuery("checkRegisteredMailAddress",Long.class).setParameter("mail_address", mail_address).getSingleResult();
+
+            em.close();
+            if(user_count>0){
+                return "入力されたアドレスはすでに登録されています。";
+            }
+        }
+        return "";
+    }
+        //社員名の必須入力チェック
+        private static String _validateName(String name){
+            if(name==null||name.equals("")){
+                return "氏名を入力してください。";
+            }
+            return "";
+        }
+        //パスワードの必須入力チェック
+        private static String _validatePassword(String password,Boolean password_check_flag){
+            if(password==null||password.equals("")){
+                return "パスワードを入力してください";
+            }
+            return "";
+        }
+
+
+
+
+
+}
